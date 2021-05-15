88d3e70ced6e1ead4af044612e404b7c9367d4bd
Aparajita-png <77184488+Aparajita-png@users.noreply.github.com>
2021-05-15 11:36:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 song="";
 scoreLeftWrist=0;
+scoreRightWrist=0;
 leftWristX=0;
 leftWristY=0;
 rightWristX=0;
@@ -21,7 +22,32 @@ console.log("modelLoaded");
 function draw(){
  fill("#FF0000");
  stroke("#000000");
- if(scoreLeftWrist>0.2){
+ if(scoreRightWrist>0.2){
+
+ 
+ circle(rightWristX,rightWristY,20);
+ if(rightWristY>0 && rightWristY<=100){
+ document.getElementById("speed").innerHTML="speed=0.5x";
+ song.rate(.5);
+ }
+ else if(rightWristY>100 && rightWristY<=200){
+    document.getElementById("speed").innerHTML="speed=1x";
+    song.rate(1);
+    }
+    else if(rightWristY>200 && rightWristY<=300){
+        document.getElementById("speed").innerHTML="speed=1.5x";
+        song.rate(1.5);
+        }
+        else if(rightWristY>300 && rightWristY<=400){
+            document.getElementById("speed").innerHTML="speed=2x";
+            song.rate(2);
+            }  
+            else if(rightWristY>400 && rightWristY<=500){
+                document.getElementById("speed").innerHTML="speed=2.5x";
+                song.rate(2.5);
+                }
+            }
+  if(scoreLeftWrist>0.2){
  circle(leftWristX,leftWristY,20);
  InNumberleftWristY=Number(leftWristY);
  remove_decimals=floor(InNumberleftWristY);
@@ -49,5 +75,8 @@ if(results.length>0){
      console.log(leftWristY);
      console.log(rightWristX);
      console.log(rightWristY);
+     scoreRighttWrist=results[0].pose.keypoints[10].score;
+     console.log(scoreRightWrist);
+
      }
      }
